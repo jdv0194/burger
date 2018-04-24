eb2b05bdfa06ca79aeb48abc538530cd60e1fa40
Juan Valdivia <juan.valdivia18@gmail.com>
2018-04-24 17:16:47 -0400

    config

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('hamburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('veggie burger', true);
+
