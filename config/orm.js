8a84459d68af0064425e76978a758e6fbf6fcbf5
Juan Valdivia <juan.valdivia18@gmail.com>
2018-04-25 17:59:46 -0400

    adding models

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,42 @@
-var connection = require("../config/connection.js");
+var connection = require("./connection.js");
 
-selectAll();
+var orm = {
+  //selectAll()
 
-insertOne();
+  selectAll: function(callback) {
+    //mySQL Query
+    connection.query("SELECT * FROM burgers", function(err, result) {
+      if (err) throw err;
+      callback(result);
+    });
+  },
 
-updateOne();
+  //insertOne()
+  insertOne: function(burger_name, callback) {
+    connection.query(
+      "INSERT INTO burgers SET ?",
+      {
+        burger_name: burger_name,
+        devoured: false
+      },
+      function(err, result) {
+        if (err) throw err;
+        callback(result);
+      }
+    );
+  },
+
+  //updateOne()
+  updateOne: function(burgerID, callback) {
+    connection.query(
+      "UPDATE burgers SET ? WHERE ?",
+      [{ devoured: true }, { id: burgerID }],
+      function(err, result) {
+        if (err) throw err;
+        callback(result);
+      }
+    );
+  }
+};
 
 module.exports = orm;
