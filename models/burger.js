f4cb66405afb467866530e4a62a5f8dde5271e39
Juan Valdivia <juan.valdivia18@gmail.com>
2018-04-25 18:36:34 -0400

    finalizing

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,5 @@
-// import orm.js into burger.js
 var orm = require("../config/orm.js");
 
-// create the code that will call the ORM functions using burger specific input for the ORM.
 var burger = {
   selectAll: function(callback) {
     orm.selectAll(function(res) {
@@ -22,5 +20,4 @@ var burger = {
   }
 };
 
-// Export at the end of the burger.js file.
 module.exports = burger;
